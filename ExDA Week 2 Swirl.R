a78532339d9787dbf1b704ee26811530c78e7474
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-06-06 18:20:39 +0530

    ggplot2 part 2

--- ExDA Week 2 Swirl.R
+++ ExDA Week 2 Swirl.R
@@ -99,10 +99,29 @@ qplot(hwy, data = mpg, facets = drv ~ ., binwidth = 2)
 
 qplot(displ, hwy, data = mpg, geom = c("point", "smooth"), facets = . ~ drv)
 
+g <- ggplot(data = mpg, aes(displ, hwy))
+## if you pu displ and hwy within quotes, aes will think that they are axes labels
 
+summary(g)
+g + geom_point() + geom_smooth(method = "lm") + facet_grid(. ~ drv) + ggtitle("Swirl Rules!")
+g + geom_point(color = "pink", size = 4, alpha = 1/2)
+g+geom_point(color="pink",size=4,alpha=1/2)
 
+g + geom_point(size = 4, alpha = 1/2, aes(color = drv))
+g + geom_point(aes(color = drv)) + labs(title = "Swirl Rules!") + labs(x = "Displacement", y = "Hwy Mileage")
+g + geom_point(aes(color = drv), size = 2, alpha = 1/2) + geom_smooth(method = "lm", se = FALSE, linetype = 3, size = 4)
+g + geom_point(aes(color = drv), size = 2, alpha = 1/2) + geom_smooth(size = 4, linetype = 3, method = "lm", se = FALSE)
+g + geom_point(aes(color = drv)) + theme_bw(base_family = "Times")
 
+plot(myx, myy, type = "l", ylim = c(-3,3))
 
+g <- ggplot(data = testdat, aes(myx, myy))
+g + geom_line() + ylim(-3, 3)
+g + geom_line() + coord_cartesian(ylim = c(-3,3))
+
+g <- ggplot(data = mpg, aes(displ, hwy, color = factor(year)))
+g + geom_point() + facet_grid(drv ~ cyl, margins = TRUE) + geom_smooth(method = "lm", se = FALSE, size = 2, color = "black") + labs(x = "Displacement", y = "Highway Mileage", title = "Swirl Rules!")
+##margins guves the totals over the columns and rows
 
 
 
