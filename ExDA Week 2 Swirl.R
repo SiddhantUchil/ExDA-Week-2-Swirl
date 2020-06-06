51904b96b3c82f03792d218f6d736f73c50fb967
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-06-06 21:30:35 +0530

    swirl ggplot 2 extras

--- ExDA Week 2 Swirl.R
+++ ExDA Week 2 Swirl.R
@@ -124,6 +124,41 @@ g + geom_point() + facet_grid(drv ~ cyl, margins = TRUE) + geom_smooth(method = 
 ##margins guves the totals over the columns and rows
 
 
+diamonds
+str(diamonds)
+qplot(price, data = diamonds, binwidth = 18497 / 30, fill = cut)
+range(diamonds$price)
+brk
+counts
+qplot(price, data = diamonds, geom = "density", color = cut)
+qplot(carat, price, data = diamonds, color = cut, aes(geom_smooth(method = "lm"))) ##this is incorrect
+
+qplot(carat, price, data = diamonds, color=cut, facets = . ~ cut) + geom_smooth(method = "lm")
+
+
+g <- ggplot(data = diamonds, aes(depth, price))
+summary(g)
+g + geom_point(alpha = 1 / 3) + facet_grid(cut ~ car2) + geom_smooth(method = "lm", size = 3, color = "pink")
+
+cutpoints <- quantile(diamonds$carat, seq(0, 1, length = 4), na.rm = TRUE)
+cutpoints
+diamonds$car2 <- cut(diamonds$carat, cutpoints)
+
+diamonds[myd, ]
+
+ggplot(data = diamonds, aes(carat, price)) + geom_boxplot() + facet_grid(. ~ cut)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
